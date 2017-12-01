14020c872e5a3cd7240a68e580c6b46d97fc968b
Darash Desai <ddesai@bu.edu>
2017-12-01 15:26:13 -0500

    Implement file chooser to select which file to analyze.

--- src/main/java/edu/bu/zaman/MMHModel/Visualizer/Visualizer.java
+++ src/main/java/edu/bu/zaman/MMHModel/Visualizer/Visualizer.java
@@ -2,9 +2,16 @@ package edu.bu.zaman.MMHModel.Visualizer;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FilenameFilter;
+import java.io.IOException;
 import java.nio.file.Paths;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileSystemView;
 
 public class Visualizer
 {		
@@ -31,39 +38,42 @@ public class Visualizer
 	public static void main(String[] args)
 	{
 		// Make sure the output directory for exporting chart data exists
-		File outputDirectory = new File(getOutputDirectory());
-		if (!outputDirectory.exists())
+		File chartDirectory = new File(getOutputDirectory());
+		if (!chartDirectory.exists())
 		{
-			outputDirectory.mkdir();
+			chartDirectory.mkdir();
 		}
 		
 		// Fetch the model output directory from the model
-		outputDirectory = new File(edu.bu.zaman.MMHModel.Simulator.Simulator.getOutputDirectory());
+		final File outputDirectory = new File(edu.bu.zaman.MMHModel.Simulator.Simulator.getOutputDirectory());
 		if (!outputDirectory.isDirectory())
 		{
 			System.out.println("Error: invalid model output directory " + outputDirectory.getAbsolutePath());
 			return;
 		}
 		
-		// List all of the output files in the model's output directory, filtered by the
-		// specified output file extension
-		String[] outputFiles = outputDirectory.list(new FilenameFilter() 
-		{
-			public boolean accept(File directory, String name)
+		JFileChooser fileChooser = new JFileChooser(outputDirectory);		
+		fileChooser.setMultiSelectionEnabled(false);
+		fileChooser.setFileFilter(new FileFilter() {
+			
+			@Override
+			public String getDescription() 
 			{
-				return name.endsWith(MODEL_FILE_EXT);
+				return "JSON";
+			}
+			
+			@Override
+			public boolean accept(File f) 
+			{
+				return f.getName().endsWith(MODEL_FILE_EXT);
 			}
 		});
 		
-		// Check to make sure that output files are available for analysis
-		if (outputFiles == null || outputFiles.length <= 0)
+		int returnValue = fileChooser.showOpenDialog(null);		
+		if (returnValue == JFileChooser.APPROVE_OPTION)
 		{
-			System.out.println("No output files available for analysis.");
-			return;
+			File selectedFile = fileChooser.getSelectedFile();
+			new DataVisualizer(selectedFile.toPath());
 		}
-		
-		String analysisFile = outputFiles[0]; // Choose a file to analyze
-		
-		new DataVisualizer(Paths.get(outputDirectory.getAbsolutePath(), analysisFile));
 	}
 }
